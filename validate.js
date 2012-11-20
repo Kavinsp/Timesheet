224a5fd970eda936b5df97985a9a75c23e3c0a26
Kavin <kavin.sp@iMac-0008.local>
2012-11-20 10:28:56 +0530

    initial commit

---
+++ validate.js
@@ -0,0 +1,36 @@
+var count =0;  
+function IsNumber(evt)
+        {
+           var charCode = (evt.which) ? evt.which : evt.keyCode
+           if (charCode > 31 && (charCode < 48 || charCode > 57))
+              return false;
+
+           return true;
+      }
+
+function NumberKey(evt)
+{
+	var charCode = (evt.which) ? evt.which : evt.keyCode
+	if(charCode < 31)
+		return false;
+	else if ((charCode >=48 && charCode <=57)||(charCode >=65 && charCode <=92) || (charCode >=97 && charCode <= 122) && (count <= 25))
+	{
+		count++;
+		return true;
+	}
+		return false;
+	
+	}
+	function alphabet(evt)
+{
+	var charCode = (evt.which) ? evt.which : evt.keyCode
+	if(charCode < 31)
+		return false;
+	else if ((charCode >=65 && charCode <=92) || (charCode >=97 && charCode <= 122) && (count <= 25))
+	{
+		count++;
+		return true;
+	}
+		return false;
+	
+	}
